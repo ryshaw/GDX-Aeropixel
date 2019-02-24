1131dfda2d9d3702b5debf45a29c3d64570ef60b
Ryan Shaw <rshaw727@gmail.com>
2019-02-23 21:45:03 -0800

    movement updated, basic map creation

--- core/src/gdx/aeropixel/Player.java
+++ core/src/gdx/aeropixel/Player.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -18,7 +19,9 @@ class Player {
     private static int rotation = 0;
 
     private static float rotSpeed = 2;
-    private static float moveSpeed = 6;
+    private static float moveSpeed = 5;
+
+    private static float speedTime = 0;
 
 
     Player() {
@@ -30,6 +33,7 @@ class Player {
 
 
     static void update() {
+        //TODO: Rotation speed lerping, similar to with the engine speed.
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             rotation -= rotSpeed;
             Game.camera.rotateAround(cameraCenter, new Vector3(0, 0, 1), -rotSpeed);
@@ -39,16 +43,41 @@ class Player {
             Game.camera.rotateAround(cameraCenter, new Vector3(0, 0, 1), rotSpeed);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            float xDelta = -moveSpeed * (float) Math.sin(Math.toRadians(rotation));
-            float yDelta = moveSpeed * (float) Math.cos(Math.toRadians(rotation));
-
-            position.add(xDelta, yDelta);
-            Game.camera.translate(xDelta, yDelta);
-            cameraCenter.add(xDelta, yDelta, 0);
+            speedTime += 0.8*Gdx.graphics.getDeltaTime();
+        } else {
+            speedTime -= 0.8*Gdx.graphics.getDeltaTime();
         }
+        speedTime = MathUtils.clamp(speedTime, 0, 1);
+        moveSpeed = MathUtils.lerp(5, 10, speedTime);
 
+        move();
         sprite.setCenter(position.x, position.y);
         sprite.setRotation(rotation);
+    }
+
+    private static void move() {
+        //TODO: Camera offset for plane when going fast.
+        float xDelta = -moveSpeed * (float) Math.sin(Math.toRadians(rotation));
+        float yDelta = moveSpeed * (float) Math.cos(Math.toRadians(rotation));
+        float xPush = speedTime * (float) Math.sin(Math.toRadians(rotation));
+        float yPush = speedTime * (float) Math.cos(Math.toRadians(rotation));
+        Vector2 offset = new Vector2(xPush, yPush);
+
+        position.add(xDelta, yDelta);
+        Game.camera.translate(xDelta, yDelta);
+        cameraCenter.add(xDelta, yDelta, 0);
+    }
+
+    static float getMoveSpeed() {
+        return moveSpeed;
+    }
+
+    static float getRotation() {
+        return rotation;
+    }
+
+    static Vector2 getCenter() {
+        return position;
     }
 
 
